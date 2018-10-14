3320e34a81e32d0a90a746ec7e83d08303a75abd
Shougo <Shougo.Matsu@gmail.com>
2018-10-14 22:51:43 +0900

    Remove debug messages (#10)
    * Remove debug messages
    
    * Improve ispath check

--- rplugin/python3/denite/filter/matcher/fruzzymatcher.py
+++ rplugin/python3/denite/filter/matcher/fruzzymatcher.py
@@ -1,5 +1,6 @@
 from ..base import Base
 from denite.util import convert2fuzzy_pattern
+from denite.util import relpath
 import os
 import sys
 import logging
@@ -22,8 +23,7 @@ class Filter(Base):
         self.name = 'matcher/fruzzy'
         self.description = 'fruzzy - freakishly fast fuzzy matcher'
         self.useNative = False
-        un = self.vim.vars["fruzzy#usenative"]
-        if un > 0:
+        if self.vim.vars.get("fruzzy#usenative", 0):
             try:
                 import fruzzy_mod
                 self.nativeMethod = fruzzy_mod.scoreMatchesStr
@@ -41,21 +41,14 @@ class Filter(Base):
                 self.debug("and then check if you have fruzzy_mod.so or fruzzy_mod.pyd at %s" %
                            pkgPath)
                 self.useNative = False
-        self.debug("usenative: %s" % self.useNative)
+        # self.debug("usenative: %s" % self.useNative)
 
     def filter(self, context):
         candidates = context['candidates']
         qry = context['input']
         # self.debug("source: %s" % candidates[0]['source_name'])
         # self.debug("context: %s" % context)
-        ispath = False
-        for s in context['sources']:
-            if s['name'] in ["file", "file_rec",
-                             "file_mru", "directory",
-                             "directory_mru", "file_old",
-                             "directory_rec", "buffer"]:
-                ispath = True
-                break
+        ispath = candidates and 'action__path' in candidates[0]
         # self.debug("candidates %s %s" % (qry, len(candidates)))
         limit = context['winheight']
         limit = int(limit) if isinstance(limit, str) else limit
@@ -75,12 +68,7 @@ class Filter(Base):
         relname = ""
         if ispath and buffer > 0 and q == "":
             fname = self.vim.buffers[buffer].name
-            d = self.vim.command("pwd")
-            try:
-                relname = path.relpath(fname, start=d)
-            except ValueError:
-                relname = fname
-                self.debug("buffer: %s, '%s'" % (relname, d))
+            relname = relpath(self.vim, fname)
         if self.useNative:
             idxArr = self.nativeMethod(q, [key(d) for d in c],
                                        relname, limit, ispath)
